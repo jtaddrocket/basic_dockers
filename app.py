358460cc57fcd76ad7e1aad79c0a24d4f1887b87
datnt1 <tiendat17.work@gmail.com>
2025-08-19 14:18:41 +0700

    docker compose

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 ## Flask app for Hello World 
 
+import time 
+
+import redis 
+
 from flask import Flask 
 import os 
+
 app=Flask(__name__)
+cache=redis.Redis(host='redis',port=6379)
 
-@app.route('/',methods=['GET'])
-def home():
-    return "Hello World!"
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            # cache.reset_retry_count()
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -=1
+            time.sleep(0.5)
 
-if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=1710)
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello DetLin! I have been seen {} times.\n'.format(count)
